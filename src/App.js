72223bc5dd7376aefe3b62e80feb4b8cf6ce1ad5
Thomas Carvalho <thomascaraujo19@gmail.com>
2024-03-06 21:29:25 -0300

    UseState

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import './App.css';
 
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
+import Home from './pages/home';
+import About from './pages/About';
+
 function App() {
   return (
     <div className="App">
+      <h1>React Hooks</h1>
+
+      <BrowserRouter >
+        <ul>
+          <li>
+            <Link to = "/">Home</Link>
+          </li>
+          <li>
+            <Link to = "/about">Sobre</Link>
+          </li>
+        </ul>
       
+      </BrowserRouter>
+    
     </div>
   );
 }
